23bdf17157aa748bf0dfd75cf14efd059875d897
yuranest <yuranest@gmail.com>
2025-05-30 22:12:27 +0300

    fix: Integrated helmet for security

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
 const routes = require("./routes");
 
 const app = express();
-const PORT = 3001 || process.env;
+const PORT = process.env.PORT || 3001;
+
+app.use(helmet());
+app.use(express.json());
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => {
-    console.log("Connected to DB");
+    if (process.env.NODE_ENV !== "production") {
+      console.log("Connected to DB");
+    }
   })
   .catch(console.error);
-
-app.use(express.json());
 
 app.use((req, res, next) => {
   req.user = {
@@ -24,5 +28,7 @@ app.use((req, res, next) => {
 app.use(routes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  if (process.env.NODE_ENV !== "production") {
+    console.log(`Server is running on port ${PORT}`);
+  }
 });
