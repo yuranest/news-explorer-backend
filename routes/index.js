44e7df5c7123494a44b5f87c449db9b6ff07d366
yuranest <yuranest@gmail.com>
2025-05-30 22:05:00 +0300

    fix: use constant for 404 in router fallback

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
 const itemRoutes = require("./clothingItem");
+const { ERROR_NOT_FOUND } = require("../utils/errors");
 
 router.use("/users", userRoutes);
 router.use("/items", itemRoutes);
 
 router.use("*", (req, res) => {
-  res.status(404).send({ message: "Requested resource not found" });
+  res.status(ERROR_NOT_FOUND).send({ message: "Requested resource not found" });
 });
 
 module.exports = router;
