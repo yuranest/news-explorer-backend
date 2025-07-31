81e2487350547c69c89e764f0b9c027748326b9f
yuranest <yuranest@gmail.com>
2025-07-31 18:41:39 +0300

    update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,4 @@
 const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("../utils/config");
 const UnauthorizedError = require("../errors/UnauthorizedError");
 
 module.exports = (req, res, next) => {
